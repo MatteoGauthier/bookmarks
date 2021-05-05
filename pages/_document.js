dd1b5e5d9b1e6023a0dd8cca2866d74db0fed307
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-05-05 09:13:54 +0200

    Adding ga.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,21 @@ class MyDocument extends Document {
 			<Html lang="fr">
 				<Head>
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
+
 					<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500&display=swap" rel="stylesheet" />
+<script
+                        async
+                        src="https://www.googletagmanager.com/gtag/js?id=G-6RSQQD3GZ5"
+                    ></script>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: ` window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-6RSQQD3GZ5');`
+                        }}
+                    />
 				</Head>
 				<body className="transition-colors duration-200 bg-white dark:bg-black">
 					<Main />
