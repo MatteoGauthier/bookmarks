bddb403a8933390287389154f0f89d5e3e31ed8a
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-05-14 00:50:10 +0200

    Modif on sitemap config

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 
-const prettier = require("prettier");
 
 (async () => {
 	const formatYmd = (date) => date.toISOString().slice(0, 10);
@@ -10,22 +9,20 @@ const prettier = require("prettier");
 	const sitemap = `
         <?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-                         <url>
-                            <loc>https://bookmarks.squale.agency</loc>
-                            <lastmod>${formatYmd(new Date())}</lastmod>
+            <url>
+                <loc>https://bookmarks.squale.agency</loc>
+                <lastmod>${formatYmd(new Date())}</lastmod>
 
-                            <changefreq>weekly</changefreq>
+                <changefreq>weekly</changefreq>
 
-                            <priority>1.0</priority>
+                <priority>1.0</priority>
 
-                        </url>
+            </url>
         </urlset>
     `;
 
 	// If you're not using Prettier, you can remove this.
-	const formatted = prettier.format(sitemap, {
-		parser: "html",
-	});
 
-	fs.writeFileSync("public/sitemap.xml", formatted);
+
+	fs.writeFileSync("public/sitemap.xml", sitemap);
 })();
