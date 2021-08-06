a89892d35f8ac48f7c15e8d7c4649f49fa1ce5d9
Mattèo Gauthier <matteo.gauthier@gmail.com>
2021-08-06 12:35:47 +0200

    added gtm instead of ga

--- pages/_document.js
+++ pages/_document.js
@@ -38,7 +38,7 @@ class MyDocument extends Document {
 						content="bookmarks, liste, squale, Mattèo Gauthier, Candice Fradet, Mattèo, ressources, design"
 					/>
 					<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500&display=swap" rel="stylesheet" />
-					<script async src="https://www.googletagmanager.com/gtag/js?id=G-6RSQQD3GZ5"></script>
+					{/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-6RSQQD3GZ5"></script>
 					<script
 						dangerouslySetInnerHTML={{
 							__html: ` window.dataLayer = window.dataLayer || [];
@@ -47,7 +47,17 @@ class MyDocument extends Document {
             
               gtag('config', 'G-6RSQQD3GZ5');`,
 						}}
+					/> */}
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+							new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+							j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+							'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+							})(window,document,'script','dataLayer','GTM-N78JKD7');`,
+						}}
 					/>
+					
 					<script
 						type="application/ld+json"
 						dangerouslySetInnerHTML={{
@@ -71,6 +81,8 @@ class MyDocument extends Document {
 
 				</Head>
 				<body className="transition-colors duration-200 bg-white dark:bg-black">
+				<noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N78JKD7"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
 					<Main />
 					<NextScript />
 				</body>
